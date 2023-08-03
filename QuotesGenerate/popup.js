f28626d0ea97e5944de0762988b87d7092f3348f
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-08-04 00:09:41 +0530

    Update popup.js

--- QuotesGenerate/popup.js
+++ QuotesGenerate/popup.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.quotable.io/quotes/random';
+const API_URL = 'https://gist.githubusercontent.com/tusuii/60d95a6b0214a679956502980b3f55d7/raw/100d6206de65d9f90327e07af9a29ea1649b0395/quote.json';
 
 function fetchData() {
   fetch(API_URL)
@@ -9,9 +9,13 @@ function fetchData() {
       return response.json();
     })
     .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomQuote = data[randomIndex];
+
+      // quoteElement.textContent = randomQuote.quote;
       const content = data[0].content;
       const quote = document.getElementById('quote');
-      quote.innerHTML = content;
+      quote.innerHTML = randomQuote.quote;
     })
     .catch(error => {
       console.error('Error fetching data:', error);
@@ -19,4 +23,3 @@ function fetchData() {
 }
 
 fetchData();
-
